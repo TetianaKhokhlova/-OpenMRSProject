d7196e3e6ccb1c9f9a3a60be155203fedf4f8efb
aliyecandir <candiraliyee@gmail.com>
2024-02-16 16:16:58 +0100

    commit

--- src/test/java/Us_410Page.java
+++ src/test/java/Us_410Page.java
@@ -4,11 +4,19 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 
-public class Us_410Page extends ReusableMethods{
+public class Us_410Page{
     public Us_410Page(){
-        PageFactory.initElements(ReusableMethods.driver,this);
+        PageFactory.initElements(DriverClass.getDriver(),this);
     }
 
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement userNameInput;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passWordInput;
+    @FindBy(xpath = "//li[@id='Pharmacy']")
+    public WebElement pharmacyLocation;
+    @FindBy(xpath = "//input[@id='loginButton']")
+    public WebElement logInButton;
     @FindBy(xpath = "//a[@id='appointmentschedulingui-homeAppLink-appointmentschedulingui-homeAppLink-extension']")
     public WebElement appointmentSchedulingButton;
     @FindBy(xpath = "//a[@id='appointmentschedulingui-manageAppointments-app']")
